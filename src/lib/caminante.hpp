1393e49e499376dd003a505da031c755a79951c9
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-12-07 17:31:31 +0100

    documenatod desarrollo de dividir

--- src/lib/caminante.hpp
+++ src/lib/caminante.hpp
@@ -2,12 +2,14 @@
 using namespace std;
 const int  CITY_MAX =500;
 const int CAM_MAX = 100 ;
+const int UPGRADE_POB =0;
+const int ALL_POB =1;
 
 class Caminante
 {
 private:
     
-    friend void Poblacion::calcFit();
+    friend void Poblacion::calcFit(Caminante &caminante);
     int camino[CITY_MAX];
     int fitness;
 public:
@@ -77,15 +79,27 @@ private:
         
 
     public:
+        //constructo predeterminado
+        Poblacion();
+        //constructo para inicializar una poblacion a partir de un archivo de texto
+        //con una matriz, se le debe de undicar el numero de caminantes de la pobaclion 
+        //con maximo CAM_MAX
         Poblacion(int caminantes, string entrada = "entrada.txt");
+        //Tras recibir una poblacion de un cliente/servidor, usar este constructor para inicilizar
         Poblacion(string data);
         ~Poblacion();
+        //tras recibir los caminantes de un cliente/servidor, esta funcion sustituye los actuales por 
+        //los nuevos
         void actualizar();
-        void calcFit();
+        //calcula el fin de un caminante y cambia su propio fit 
+        void calcFit(Caminante &caminante);
+        //divide la poblacion en n subpoblaciones
         void dividir(int n, Poblacion pobs[]);
+        //obtiene el mejor fit de la poblacion
         int mejorFit();
+        //obtiene el fites medio de la poblacion
         int mediaFit();
-        void serializar();
+        string serializar(int flg = ALL_POB);
 };
 //le indicas cuantos caminantes va a haber y la entrada donde estan los datos
 Poblacion::Poblacion(int numCam, string entrada = "entrada.txt")
@@ -103,6 +117,7 @@ Poblacion::Poblacion(int numCam, string entrada = "entrada.txt")
 Poblacion::Poblacion(string data)
 {
 
+
 }
 
 Poblacion::~Poblacion()
@@ -117,32 +132,87 @@ void actualizar(){
 }
 
 //calculas el fit de un caminante y se lo guardas
-void Poblacion::calcFit()
+void Poblacion::calcFit(Caminante &caminate)
 {
 
 }
-//divide la poblacion en n subpoblaciones y las devuelve en array
-void dividir(int n, Poblacion pobs[])
+
+
+    //divide la poblacion en n subpoblaciones y las devuelve en array
+    void
+    Poblacion::dividir(int n, Poblacion pobs[])
 {
+    int sobr= numCam%n;
+    int indx=0;
+    
+    for (int  i = 0; i < n; i++)
+    {
+        pobs[i].numCities=numCities;
+
+       
+        #warning copiar matriz  // ¿memcopy con el el tamaño?
+        int numSub = (numCam / n) - 1;
+        if(sobr>0){
+           numSub++;
+            sobr--;
+        }
+        pobs[i].numCam =numSub;
+        //¿optimizar el bucle con memcopy?
+        for (int j = 0; j < numSub; j++)
+        {
+            pobs[i].caminantes[j] = caminantes[j+indx];
+        }
+        indx += numSub;
+    }
+    
+
 
 }
 
 //obtiene el mejor fitnes de la poblacion
-int mejorFit()
+int Poblacion::mejorFit()
 {
 
 }
 
 //obtiene la media de todos los fits de la poblacion
-int mediaFit()
+int Poblacion::mediaFit()
 {
 
 }
 
-//transforma los datos de la poblacion en un único 
-void serializar()
+//transforma los datos de la poblacion en un único string, por defecto toda
+//con UPGRADE_POB, unicamente el vector de caminantes
+//ALL_POB=>
+//UPGRADE_POB=> "numCam:[caminante]*"
+string Poblacion::serializar(int flg = ALL_POB)
 {
+    string msg;
+    switch (flg)
+    {
+        case ALL_POB:
+            msg = to_string(numCities) + ":";
+            #warning serializar matriz
+            msg += to_string(numCam) + ":";
+            for (int i = 0; i < numCam; i++)
+            {
+                msg += caminantes[i].serializar() + ";";
+            }
+            
 
+            break;
+        case UPGRADE_POB:
+        msg=to_string(numCam)+":";
+            for (int i = 0; i < numCam ; i++)
+            {
+                msg+=caminantes[i].serializar()+";";
+            }
+            return msg;
+            break;
+
+        default:
+            break;
+    } 
 }
 
 
