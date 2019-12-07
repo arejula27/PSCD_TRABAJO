35fdb6569edd7d57545a7e1bcf72672f8f6481fd
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-12-07 14:05:20 +0100

    comento las funciones

--- src/lib/caminante.hpp
+++ src/lib/caminante.hpp
@@ -6,21 +6,27 @@ const int CAM_MAX = 100 ;
 class Caminante
 {
 private:
-    /* data */
-    friend class Poblacion;
+    
+    friend void Poblacion::calcFit();
     int camino[CITY_MAX];
     int fitness;
 public:
 
     Caminante();
     Caminante(string MiCamino);
-    void ini();
+    void ini(int numCity);
     ~Caminante();
     int MyFit();
     void Mutar();
     void Cruzar(const Caminante &O1, const Caminante &O2);
     string serializar();
 };
+
+
+Caminante::Caminante()
+{  
+   
+}
 
 Caminante::Caminante(string MiCamino)
 {   
@@ -63,24 +69,36 @@ class Poblacion
 private:
 
         int dist[CITY_MAX][CITY_MAX];
+        int numCam;
         Caminante caminantes[CAM_MAX];
         string cities[CITY_MAX];
+        int numCities;
         
 
     public:
         Poblacion(int caminantes, string entrada = "entrada.txt");
         Poblacion(string data);
         ~Poblacion();
+        void actualizar();
         void calcFit();
         void dividir(int n, Poblacion pobs[]);
         int mejorFit();
         int mediaFit();
         void serializar();
 };
-
-Poblacion::Poblacion(int caminantes, string entrada = "entrada.txt")
+//le indicas cuantos caminantes va a haber y la entrada donde estan los datos
+Poblacion::Poblacion(int numCam, string entrada = "entrada.txt")
 {
+    //inicializar numCam
+    //rellenar la matriz
+    //inicializar numCities
+    for (int i = 0; i < numCam; i++)
+    {
+        caminantes[i].ini(numCities);
+    }
+    
 }
+//crea una poblacion a partir de un string con toda la info, util para el servidor
 Poblacion::Poblacion(string data)
 {
 
@@ -89,23 +107,41 @@ Poblacion::Poblacion(string data)
 Poblacion::~Poblacion()
 {
 }
-void calcFit()
+
+//actualiza los caminnates de una poblacion a partir de un string, util para 
+//el servidor
+void actualizar(){
+
+
+}
+
+//calculas el fit de un caminante y se lo guardas
+void Poblacion::calcFit()
 {
 
 }
+//divide la poblacion en n subpoblaciones y las devuelve en array
 void dividir(int n, Poblacion pobs[])
 {
 
 }
+
+//obtiene el mejor fitnes de la poblacion
 int mejorFit()
 {
 
 }
+
+//obtiene la media de todos los fits de la poblacion
 int mediaFit()
 {
 
 }
+
+//transforma los datos de la poblacion en un único 
 void serializar()
 {
 
 }
+
+
