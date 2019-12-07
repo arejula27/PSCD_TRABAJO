86b2516ce184a98c8d86cb8bfe0a4825accbbfba
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-12-07 13:22:28 +0100

    espec poblacion

--- src/lib/caminante.hpp
+++ src/lib/caminante.hpp
@@ -1,3 +1,8 @@
+#include <string>
+using namespace std;
+const int  CITY_MAX =500;
+const int CAM_MAX = 100 ;
+
 class Caminante
 {
 private:
@@ -19,17 +24,51 @@ Caminante::~Caminante()
 class Poblacion
 {
 private:
-    /* data */
-public:
-    Poblacion(/* args */);
-    ~Poblacion();
+
+        int dist[CITY_MAX][CITY_MAX];
+        Caminante caminantes[CAM_MAX];
+        string cities[CITY_MAX];
+        
+
+    public:
+        Poblacion(int caminantes, string entrada = "entrada.txt");
+        Poblacion(string data);
+        ~Poblacion();
+        void calcFit();
+        void dividir(int n, Poblacion pobs[]);
+        int mejorFit();
+        int mediaFit();
+        void serializar();
 };
 
-Poblacion::Poblacion(/* args */)
+Poblacion::Poblacion(int caminantes, string entrada = "entrada.txt")
 {
+}
+Poblacion::Poblacion(string data)
+{
+
 }
 
 Poblacion::~Poblacion()
 {
 }
+void calcFit()
+{
 
+}
+void dividir(int n, Poblacion pobs[])
+{
+
+}
+int mejorFit()
+{
+
+}
+int mediaFit()
+{
+
+}
+void serializar()
+{
+
+}
