c916f3033aa1e4e3ebcb359a37adf288563a40bd
aaronainzon17 <aaronainzon@gmail.com>
2019-12-15 21:14:15 +0100

    pruebaCommitJeje(monitorEstadistico)

---
+++ src/client/Monitorizacion.hpp
@@ -0,0 +1,20 @@
+#ifndef MONITORIZACION
+#define MONITORIZACION
+
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+class Monitorizacion{
+
+    public:
+
+
+    private:
+        mutex mtx;
+
+};
+#endif
