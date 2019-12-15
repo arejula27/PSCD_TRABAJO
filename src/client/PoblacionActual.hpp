fed022dacb9fec71a403143551a03b19a12b89ca
aaronainzon17 <aaronainzon@gmail.com>
2019-12-15 21:16:12 +0100

    yaQmePongoPrueboTodo(monitorPoblacion)

--- src/client/PoblacionActual.hpp
+++ src/client/PoblacionActual.hpp
@@ -1,5 +1,32 @@
-monitor poblacionActual
-    operacion init(ent string entrada:= entrada.txt)
+#ifndef POBLACION_ACTUAL
+#define POBLACION_ACTUAL
+
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+/*monitor poblacionActual
+    operacion init(ent string entrada:= entrada.txt)*/
+class PoblacionActual{
+    public:
+
+
+
+    private:
+
+
+
+};
+
+
+
+#endif
+
+
+
 
 
 
