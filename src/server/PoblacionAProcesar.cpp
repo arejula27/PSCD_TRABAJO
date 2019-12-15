19db741662c2c56ae34d2f6cc570d0e886c3c182
HectorRute98 <57754377+HectorRute98@users.noreply.github.com>
2019-12-15 23:11:48 +0100

    Actualizacion

---
+++ src/server/PoblacionAProcesar.cpp
@@ -0,0 +1,41 @@
+//*****************************************************************
+// File:	PoblacionAprocesar.cpp
+// Date:	Diciembre 2019
+//*****************************************************************
+
+#include "PoblacionAProcesar.hpp"
+
+//----------------------------------------------------------
+PoblacionAProcesar::PoblacionAProcesar(string poblacion_) {
+	this->poblacion = poblacion_;
+
+    for(int i=0; i<numCaminantes; i++) {
+        //caminantes[i] = obtener de poblacion
+    }
+};
+
+
+//----------------------------------------------------------
+// funcion seleccionar
+void PoblacionAProcesar::seleccionar() {
+    unique_lock<mutex> lck(mtx);
+
+}
+
+//----------------------------------------------------------
+// funcion cruzar
+void PoblacionAProcesar::cruzar() {     // Poner parametros para llamar a cruzar
+    unique_lock<mutex> lck(mtx);
+    caminante.Cruzar(&o1,&o2);
+
+}
+
+//----------------------------------------------------------
+// funcion mutar   
+void PoblacionAProcesar::mutar() {
+    unique_lock<mutex> lck(mtx);
+    caminante.Mutar();
+
+}
+
+//----------------------------------------------------------
