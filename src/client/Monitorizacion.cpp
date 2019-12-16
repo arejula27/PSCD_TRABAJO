a80349bbb069e42d447890c7fafa6fbf4723bc9c
Merni27 <marcoshernando123@gmail.com>
2019-12-16 23:03:16 +0100

    Monitorizacion a medias

--- src/client/Monitorizacion.cpp
+++ src/client/Monitorizacion.cpp
@@ -1,6 +1,7 @@
 #include "caminante.hpp"
 #include "Monitorizacion.hpp"
 
+
 Monitorizacion:: Monitorizacion(){
 
 }
@@ -9,15 +10,44 @@ Monitorizacion:: ~Monitorizacion(){
 
 }
 
-void Monitorizacion::calculoFit(Caminante &caminante){
+void Monitorizacion::codificar(Poblacion pob, int flg){
     unique_lock <mutex> lck(mtx);
+    pob.codificar(flg);
+
 }
 
-int Monitorizacion::mejorFit(){
+
+void Monitorizacion::descodificar(Poblacion pob,string msg,int flg){
     unique_lock <mutex> lck(mtx);
-    Poblacion.mejorFit();
+    pob.descodificar(msg,flg);
+
 }
 
-int Monitorizacion::avgFit(){
-    unique_lock <mutex> lck(mtx); 
+
+
+
+Poblacion Monitorizacion::crearPob(int n){
+    unique_lock <mutex> lck(mtx);
+    //No se con cual de las tres ha que llamar
+
+}
+
+void Monitorizacion::dividir(Poblacion pob,int n,Poblacion pobs[]){
+    unique_lock <mutex> lck(mtx);
+    pob.dividir(n,pobs);
+}
+
+Poblacion Monitorizacion::fusionar(Poblacion pobs[]){
+    
+    //No tengo claro como hacerla
+}
+
+int Monitorizacion::mejorFit(Poblacion pob){
+    unique_lock <mutex> lck(mtx);
+    pob.mejorFit();
+}
+
+int Monitorizacion::mediaFit(Poblacion pob){
+    unique_lock <mutex> lck(mtx);
+    pob.mediaFit(); 
 }
--- src/client/Monitorizacion.hpp
+++ src/client/Monitorizacion.hpp
@@ -9,17 +9,47 @@
 
 using namespace std;
 
+//-----------------------------------------------------
+//  Compilación condicional: ¿quiero hacer "logging"? Modo no-main: var "logger" se declarará en el "main"
+#ifdef LOGGING_MODE
+    #include <Logger.hpp>
+    extern Logger logger;
+#else
+    #ifndef ADD_EVENT //para evitar redefiniciones
+        #define ADD_EVENT(e)  // nada
+    #endif
+#endif
+
 class Monitorizacion{
     friend Caminante;
 public:
+    //Construye el monitor
     Monitorizacion();
+    //Destructor
     ~Monitorizacion();
-    void calculoFit(Caminante &caminante);
-    int mejorFit();
-    int avgFit();
+    //Crea poblaciones según el parámetro n
+    Poblacion crearPob(int n);
+    //Divide la poblacion en n subpoblaciones
+    void dividir(Poblacion pob,int n, Poblacion pobs[]);
+    //Devuelve una poblacionn fruto de fusionar pobs[]
+    Poblacion fusionar(Poblacion pobs[]);
+    //Devuelve el mejor fitness de la poblacion
+    int mejorFit(Poblacion pob);
+    //Devuelve la media de fitness de la poblacion
+    int mediaFit(Poblacion pob);
+    //Prepara los datos para ser enviados por el socket
+    void codificar(Poblacion pob,int flg = ALL_POB);
+    //flg: 1=ALL_POB 2=UPGRADE_POB
+    void descodificar(Poblacion pob,string mag, int flg);
 
 private:
+
     mutex mtx;
+
+    int numGen; //Número de poblaciones generadas hasta el momento
+    
+    //Condicion para que el estadistico pueda tomar los datos como validos
+    condition_variable (datosListos); //Es necesario?
     
 };
 #endif
