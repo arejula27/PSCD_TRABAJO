7d490b86d0bec2afae3c90a249b1bdf43f363ff6
HectorRute98 <57754377+HectorRute98@users.noreply.github.com>
2020-01-03 15:21:34 +0100

    Update

--- src/server/PoblacionAProcesar.hpp
+++ src/server/PoblacionAProcesar.hpp
@@ -27,9 +27,13 @@ public:
 	//  Inicialización del monitor, se le pasa el vector de caminantes para
 	//para operar sobre ellos 
 	PoblacionAProcesar(Poblacion poblacion_);
-	void seleccionar(int id);
+
+	void seleccionar();
+
 	void cruzar(int id);
+
 	void mutar(int id, int id2);
+
 	// Devuelve la poblacion
 	Poblacion getPoblacion();
 };
