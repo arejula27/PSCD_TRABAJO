694f7addcf0147edde8d74d8822243d6c3d3d417
Merni27 <marcoshernando123@gmail.com>
2020-01-11 17:11:15 +0100

    pruebas

--- src/lib/caminante/mainPbPob.cpp
+++ src/lib/caminante/mainPbPob.cpp
@@ -195,7 +195,7 @@ void sacarPantIsDivYFus(){
 }
 
  bool cityPosCorrect(string txt, int numCity){
-
+     bool res=true;
      bool cities[numCity];
      for(int i=0;i<numCity;i++){
          cities[i]=false;
@@ -204,27 +204,31 @@ void sacarPantIsDivYFus(){
      int c = stoi(&txt[0]);
       while(txt[++coma]!=',');
      int end =txt.find_first_of(":");
-     for(int i =0;i<numCity;i++){
+     for(int i =0;i<numCity+1;i++){
          int num=stoi(&txt[++coma]);
          if(!cities[num]){
              cities[num]= true;
          }
          else{
              cout<<error(ERROR,"se repiten ciudades en los caminantes,ciudad: ")<< i<<endl;
-             return false;
+             //return false;
+             res=false;
          }
          if(num == c){
             while(isDigit(txt[++coma],"h"))
+            cout<<&txt[coma]<<endl;
+            cout<<"coma"<<coma<<"--end"<<end<<endl;
             if(coma != end){
                  cout<<error(ERROR,"numero de ciudades incorrecto")<<endl;
-                 return false;
+                 //return false;
+                 res=false;
             }
          }
          while(txt[++coma]!=',');
          
 
      }
-    return true;
+    return res;
 
  }
  bool caminanteCorrecto(string txt,int numCity){
@@ -743,7 +747,7 @@ void sacarPantCamCruz()
 int main()
 {
 
-    caminanteIni();
+    //caminanteIni();
     sacarPantIsCamCyD();
     sacarPantIsPobCyD();
     sacarPantIsPCS();
