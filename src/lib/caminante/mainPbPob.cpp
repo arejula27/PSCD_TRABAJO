283f2dfbe5e2a7c3d8603b68c342e068220af095
Merni27 <marcoshernando123@gmail.com>
2020-01-13 00:58:12 +0100

    2da prueba

--- src/lib/caminante/mainPbPob.cpp
+++ src/lib/caminante/mainPbPob.cpp
@@ -565,12 +565,12 @@ void sacarPantCruzar()
 bool tryselec()
 {
     int idx = 0;
-    int numCam = 66;
+    int numCam = 70;
     int ciudadIni = 0;
     int numCiuds = 12;
     string entrada = "./../entradas/uk12.txt";
     Poblacion pob(numCam, ciudadIni, numCiuds, entrada);
-    int n=20;
+    int n=22;
     pob.seleccionar(n);
     cout<<pob.getNumCam()<<endl;
     string txt = pob.codificar();
