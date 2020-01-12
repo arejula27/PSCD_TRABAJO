8a7c9553addf5f3e6fa1761ea044a1a13d81d1c1
Merni27 <marcoshernando123@gmail.com>
2020-01-12 20:00:31 +0100

    ert

--- src/lib/caminante/caminante.cpp
+++ src/lib/caminante/caminante.cpp
@@ -748,7 +748,7 @@ void Poblacion::seleccionar(int n){
 
     for(int i=0;i<nVeces;i++){ //Bucle para cada torneo
         numElegTor=0;
-        cout<<"----INICIO TORNEO "<<i<<"----["<<k*i<<"-"<<k*(i+1)<<"]----"<<l<<endl;
+        //cout<<"----INICIO TORNEO "<<i<<"----["<<k*i<<"-"<<k*(i+1)<<"]----"<<l<<endl;
         
         while(numElegTor<l){   //Seleccion dentro de cada torneo
             j=0;
