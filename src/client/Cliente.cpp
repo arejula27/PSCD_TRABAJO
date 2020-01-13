8f0ca0a46868fc795525d4f745f9287e12411b45
Merni27 <marcoshernando123@gmail.com>
2020-01-13 12:37:27 +0100

    couts

--- src/client/Cliente.cpp
+++ src/client/Cliente.cpp
@@ -378,6 +378,7 @@ int main(int argc, char const *argv[]){
     fichero="./sh07.txt";
     PobActual pa(gen);
     cout<<"peta?"<<endl;
+    cout<<numPersonas<<"--"<<ciudIni<<"--"<<cities<<"--"<<fichero<<endl;
     Poblacion proletariado(numPersonas,ciudIni,cities,fichero);
     cout<<"genera poblacion"<<endl;
 
